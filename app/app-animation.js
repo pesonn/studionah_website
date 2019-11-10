425a4c3c8a026ac1de9e87cd49f217ee0997e2ed
pesonn <43933988+pesonn@users.noreply.github.com>
2019-11-10 17:39:47 +0100

    neue JS Datei für Animationen

---
+++ app/app-animation.js
@@ -0,0 +1,69 @@
+
+// Fenstergröße auf .grid-main beziehen nicht auf Fenstergröße
+var gridmain = document.querySelector(".grid-main"); 
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, gridmain.clientWidth / gridmain.clientHeight, 0.1, 600);
+
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(gridmain.clientWidth, gridmain.clientHeight);
+renderer.setClearColor("#f5f5f5");
+renderer.setPixelRatio(1.55);
+
+gridmain.appendChild(renderer.domElement);
+
+
+// muss auf window bleiben, da dieses die Größe steuert
+window.addEventListener("resize", () => {
+  renderer.setSize(gridmain.clientWidth, gridmain.clientHeight);
+  camera.aspect = gridmain.clientWidth / gridmain.clientHeight;
+  
+  camera.updateProjectionMatrix();
+});
+
+
+camera.position.z = 400;
+
+// /* ORBIT CONTROLS */
+
+// var controls = new THREE.OrbitControls(camera);
+// controls.enableZoom = false;
+// controls.enabelDumping = true;
+// controls.autoRotate = false;
+
+
+/* TRACKBALL CONTROLS */
+
+var controls = new THREE.TrackballControls(camera);
+
+
+var loader = new THREE.FontLoader();
+loader.load("fonts/helvetiker_regular.typeface.json", function(font) {
+
+  var text = new THREE.TextGeometry("studio nah", {
+    font: font,
+    size: 60,
+    height: 12,
+  });
+
+  text.scale
+
+  var textMaterial = new THREE.MeshBasicMaterial({color: 0x353535, reflectivity: 0});
+  var mesh = new THREE.Mesh(text, textMaterial);
+  scene.add(mesh);
+  text.center();
+   
+});
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
+
+
+
+
+
