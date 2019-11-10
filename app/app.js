777a88369acf6408941a8f5ecfa6de0f8c617e58
pesonn <43933988+pesonn@users.noreply.github.com>
2019-11-10 20:48:58 +0100

    Fade Animationen Burger Menu funktionieren

--- app/app.js
+++ app/app.js
@@ -1,7 +1,103 @@
-const burgerMenuIcon = document.querySelector(".burger-menu");
-
 const gridNavBurger = document.querySelector(".grid-nav-burger");
 
+const burgerMenuIcon = document.querySelector(".burger-menu");
+
+const burgerMenuLines = document.querySelectorAll(".line");
+
+const navItems = document.querySelectorAll(".nav__item");
+
 burgerMenuIcon.addEventListener("click", () => {
-  console.log("hello");
+
+  const burgerOverlay =  document.querySelector(".burger-nav__overlay");
+  const burgerNavOverlay = document.createElement("div");
+  burgerNavOverlay.setAttribute("class", "burger-nav__overlay");
+  //Wechsel zwischen erstellen und löschen
+
+  if (!burgerOverlay) {
+  console.log("Yummy Burger!");
+    // DIV .burger-nav__Overlay erstellen
+  burgerNavOverlay.setAttribute("class", "burger-nav__overlay");
+
+  // alle benötigten Klassen in DIV verschieben
+
+
+  burgerMenuLines[0].classList.add("burger-fadein-1__line1");
+  burgerMenuLines[1].classList.add("burger-fadein-1__line2");
+  burgerMenuLines[2].classList.add("burger-fadein-1__line3");
+
+  window.setTimeout( () => {
+    burgerNavOverlay.appendChild(burgerMenuIcon);
+    burgerNavOverlay.appendChild(navItems[0]);
+    burgerNavOverlay.appendChild(navItems[1]);
+    burgerNavOverlay.appendChild(navItems[2]);
+    
+    gridNavBurger.appendChild(burgerNavOverlay);
+
+    burgerNavOverlay.classList.add("overlay-fadein");
+  }, 300);
+
+  window.setTimeout( () => {
+    burgerMenuLines[0].classList.add("burger-fadein-2__line1");
+    burgerMenuLines[1].classList.add("burger-fadein-2__line2");
+    burgerMenuLines[2].classList.add("burger-fadein-2__line3");
+  }, 1200)
+  }
+  else {
+    console.log("Klick nicht so viel!");
+    
+    burgerMenuLines[0].classList.remove("burger-fadein-1__line1");
+    burgerMenuLines[1].classList.remove("burger-fadein-1__line2");
+    burgerMenuLines[2].classList.remove("burger-fadein-1__line3");
+    burgerMenuLines[0].classList.remove("burger-fadein-2__line1");
+    burgerMenuLines[1].classList.remove("burger-fadein-2__line2");
+    burgerMenuLines[2].classList.remove("burger-fadein-2__line3");
+  
+    burgerMenuLines[0].classList.add("burger-fadeout-1__line1");
+    burgerMenuLines[1].classList.add("burger-fadeout-1__line2");
+    burgerMenuLines[2].classList.add("burger-fadeout-1__line3");
+    console.log("0")
+
+    // muss getrennt laufen, da es sonst nicht funktioniert...
+    window.setTimeout( () => {
+      burgerOverlay.classList.remove("overlay-fadein");
+    }, 250);
+
+    window.setTimeout( () => {
+      burgerOverlay.classList.add("overlay-fadeout"); 
+      console.log("1")
+    }, 250);
+
+    window.setTimeout( () => {
+      burgerOverlay.classList.remove("overlay-fadeout");
+      gridNavBurger.appendChild(burgerMenuIcon);
+      gridNavBurger.appendChild(navItems[0]);
+      gridNavBurger.appendChild(navItems[1]);
+      gridNavBurger.appendChild(navItems[2]);
+      gridNavBurger.removeChild(burgerOverlay); 
+      console.log("2") 
+    }, 250);
+
+
+    window.setTimeout( () => {
+      burgerMenuLines[0].classList.remove("burger-fadeout-1__line1");
+      burgerMenuLines[1].classList.remove("burger-fadeout-1__line2");
+      burgerMenuLines[2].classList.remove("burger-fadeout-1__line3");
+      burgerMenuLines[0].classList.add("burger-fadeout-2__line1");
+      burgerMenuLines[1].classList.add("burger-fadeout-2__line2");
+      burgerMenuLines[2].classList.add("burger-fadeout-2__line3");
+      console.log("3")
+    }, 250);
+
+    window.setTimeout( () => {
+      burgerMenuLines[0].classList.remove("burger-fadeout-2__line1");
+      burgerMenuLines[1].classList.remove("burger-fadeout-2__line2");
+      burgerMenuLines[2].classList.remove("burger-fadeout-2__line3");
+      console.log("4")
+    }, 1000);
+
+
+    
+    
+  };
+
 });
