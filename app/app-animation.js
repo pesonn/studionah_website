f17b568818a1d3af7ecb36caa8de53f4b11854a1
pesonn <43933988+pesonn@users.noreply.github.com>
2019-11-11 12:14:06 +0100

    font-color passt sich auch an

--- app/app-animation.js
+++ app/app-animation.js
@@ -7,7 +7,28 @@ var camera = new THREE.PerspectiveCamera(75, gridmain.clientWidth / gridmain.cli
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(gridmain.clientWidth, gridmain.clientHeight);
-renderer.setClearColor("#f5f5f5");
+
+// setzt die Farbe beim ersten Laden der Seite
+function setInitialBackgroundColor() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+    renderer.setClearColor("#000");
+  }
+  else {
+    renderer.setClearColor("#f5f5f5");
+  }
+};
+setInitialBackgroundColor();
+
+//ändert die Farbe während man auf der Seite Ist
+window.matchMedia("(prefers-color-scheme: light)").addListener(() => {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+    renderer.setClearColor("#000");
+  }
+  else {
+    renderer.setClearColor("#f5f5f5");
+  }
+});
+
 renderer.setPixelRatio(1.55);
 
 gridmain.appendChild(renderer.domElement);
@@ -40,6 +61,25 @@ var controls = new THREE.TrackballControls(camera);
 var loader = new THREE.FontLoader();
 loader.load("fonts/helvetiker_regular.typeface.json", function(font) {
 
+  var textMaterial;
+  var setTextMaterial = function() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+      textMaterial = new THREE.MeshBasicMaterial({color: 0xf5f5f5, reflectivity: 0});
+      console.log(textMaterial);
+    }
+    else {
+      textMaterial = new THREE.MeshBasicMaterial({color: 0x000, reflectivity: 0});
+      console.log("2.");
+    }
+  };
+
+  setTextMaterial();
+
+  
+
+console.log(textMaterial);
+
+
   var text = new THREE.TextGeometry("studio nah", {
     font: font,
     size: 60,
@@ -47,9 +87,24 @@ loader.load("fonts/helvetiker_regular.typeface.json", function(font) {
   });
 
   text.scale
+  var mesh = new THREE.Mesh(text, textMaterial);;
+   window.matchMedia("(prefers-color-scheme: light)").addListener(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+      setTextMaterial();
+      mesh = new THREE.Mesh(text, textMaterial);
+      scene.add(mesh);
+      console.log("3.");
+    }
+    else {
+      setTextMaterial();
+      mesh = new THREE.Mesh(text, textMaterial);
+      scene.add(mesh);
+      console.log("4.");
+    }
+  });
 
-  var textMaterial = new THREE.MeshBasicMaterial({color: 0x353535, reflectivity: 0});
-  var mesh = new THREE.Mesh(text, textMaterial);
+  // var textMaterial = new THREE.MeshBasicMaterial({color: 0x353535, reflectivity: 0});
+  // var mesh =  new THREE.Mesh(text, textMaterial);
   scene.add(mesh);
   text.center();
    
